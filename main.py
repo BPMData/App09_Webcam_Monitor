a55bb7b5a02deefa84888d9ef5015c75155a172d
BPMData <bryan.patrick.a.murphy@gmail.com>
2023-07-07 14:54:56 -0400

    Got the basic "livestream from your webcam" functionality down!

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import cv2
 import time
 
+# livefeed = cv2.VideoCapture(0)
+# check, frame = livefeed.read()
+# time.sleep(1)
+# cv2.imwrite("testframe.png", frame)
+# time.sleep(1)
+# check, frame = livefeed.read()
+# time.sleep(1)
+# cv2.imwrite("testframe2.png", frame)
+# time.sleep(1)
+# check, frame = livefeed.read()
+# time.sleep(1)
+# cv2.imwrite("testframe3.png", frame)
+# time.sleep(1)
+
 livefeed = cv2.VideoCapture(0)
-check, frame = livefeed.read()
-time.sleep(1)
-cv2.imwrite("testframe.png", frame)
-time.sleep(1)
-check, frame = livefeed.read()
-time.sleep(1)
-cv2.imwrite("testframe2.png", frame)
-time.sleep(1)
-check, frame = livefeed.read()
-time.sleep(1)
-cv2.imwrite("testframe3.png", frame)
-time.sleep(1)
+time.sleep(3)
+
+while True:
+    check, frame = livefeed.read()
+    cv2.imshow("My Video", frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord("q"):
+        break
+
+livefeed.release()
